cd414b2d795053d0ea2dc036ba4e4ed7aab9ca34
jack_qin <qfl2000@gmail.com>
2017-03-13 23:06:57 +0800

    for mqtt websocket

--- run-mqtt.sh
+++ run-mqtt.sh
@@ -1,6 +1,6 @@
 
 docker run -d                   \
-    -p 1883:1883                \
-    -v `pwd`/conf:/etc/rabbitmq \
+    -p 1883:1883 -p 15675:15675 \
+    -v `pwd`/opt:/opt           \
     rabbitmq
 
